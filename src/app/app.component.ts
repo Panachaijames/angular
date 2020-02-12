b7d1a13982cd106434ce75b73e9f2bafb23abfe5
PANACHAI THONGVINIT <jameslovepe@hotmail.com>
2020-02-12 15:05:39 +0000

    Lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  name:string = "James";
+  age:number = 20;
+  
 }
+
